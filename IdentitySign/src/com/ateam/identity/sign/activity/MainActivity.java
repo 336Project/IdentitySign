5b1291933fb441ab4470066173301115ff84170f
lixiaowei <lixiaowei@91yong.com>
2015-10-14 19:39:57 +0800

    主页二代身份证自动读取逻辑编写

--- IdentitySign/src/com/ateam/identity/sign/activity/MainActivity.java
+++ IdentitySign/src/com/ateam/identity/sign/activity/MainActivity.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,6 +29,7 @@ import com.ateam.identity.sign.MyApplication;
 import com.ateam.identity.sign.R;
 import com.ateam.identity.sign.access.SignAccess;
 import com.ateam.identity.sign.access.I.HRequestCallback;
+import com.ateam.identity.sign.dao.StudentDao;
 import com.ateam.identity.sign.dao.UnCommitInfoDao;
 import com.ateam.identity.sign.moduel.HBaseObject;
 import com.ateam.identity.sign.moduel.SignObject;
@@ -48,6 +53,13 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 	private boolean isSign = false;//是否正在签到
 	
 	private ImageView rightIcon;
+	
+	private ImageView mSignImg;
+	private TextView mSignName;
+	private TextView mSignNum;
+	private TextView mSignSex;
+	
+	private String currClassroom;//当前所在班级
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -82,6 +94,11 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 		MyApplication application = (MyApplication) getApplication();
 		User user = application.getUser();
 		
+		mSignImg = (ImageView) findViewById(R.id.iv_sign_img);
+		mSignName = (TextView) findViewById(R.id.tv_sign_name);
+		mSignNum = (TextView) findViewById(R.id.tv_sign_num);
+		mSignSex = (TextView) findViewById(R.id.tv_sign_sex);
+		
 		mTextViewName = (TextView) findViewById(R.id.tv_name);
 		mTextViewCard = (TextView) findViewById(R.id.tv_card);
 		mTextViewSex = (TextView) findViewById(R.id.tv_sex);
@@ -112,7 +129,7 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 			mLinearLayoutClass.addView(radioGroup);
 		}
 		
-		findViewById(R.id.btn_sign_second).setOnClickListener(this);
+		//findViewById(R.id.btn_sign_second).setOnClickListener(this);
 		findViewById(R.id.btn_sign_third).setOnClickListener(this);
 		findViewById(R.id.btn_sign_manul).setOnClickListener(this);
 		initSign();
@@ -135,6 +152,8 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 				MyToast.showShort(MainActivity.this, "签到成功!");
 				if(!result.isSuccess()){
 					saveSign();
+				}else{
+					postRunnable();
 				}
 			}
 			
@@ -150,24 +169,47 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 			@Override
 			public void onReadSuccess(People people) {
 				playRing();
-				//签到
-				List<SignObject> signList = new ArrayList<SignObject>();
-				signObject = new SignObject(people.getPeopleIDCode(), SysUtil.getNowTime());
-				signList.add(signObject);
-				mAccess.sign(signList);
+				updatePeople(people);
+				StudentDao studentDao = new StudentDao(MainActivity.this);
+				if(studentDao.isCanSign(mSignNum.getText().toString(), mTextViewCard.getText().toString(), currClassroom)){
+					//签到
+					List<SignObject> signList = new ArrayList<SignObject>();
+					signObject = new SignObject(people.getPeopleIDCode(), SysUtil.getNowTime());
+					signList.add(signObject);
+					mAccess.sign(signList);
+				}else{
+					isSign = false;
+					MyToast.showShort(MainActivity.this, "抱歉，你不是该班级的人员");
+					postRunnable();
+				}
 			}
 			
 			@Override
 			public void onReadFail(int code) {
 				isSign = false;
+				postRunnable();
 			}
 		};
+	}
+
+	@SuppressWarnings("deprecation")
+	private void updatePeople(People people) {
+		if(people != null){
+			if (people.getPhoto() != null) {
+				Bitmap photo = BitmapFactory.decodeByteArray(people.getPhoto(), 0,
+						people.getPhoto().length);
+				mSignImg.setBackgroundDrawable(new BitmapDrawable(photo));
+			}
+			mSignName.setText(people.getPeopleName());
+			mSignNum.setText(people.getPeopleIDCode());
+			mSignSex.setText(people.getPeopleSex());
+		}
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.btn_sign_second:
+		/*case R.id.btn_sign_second:
 			if(isSign){
 				MyToast.showLong(this, "正在签到中，请稍等...");
 				return;
@@ -175,12 +217,13 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 			isSign = true;
 			asyncSFZ = new AsyncParseSFZ(this,onReadSFZListener);
 			asyncSFZ.execute(SFZ.SECOND);
-			break;
+			break;*/
 		case R.id.btn_sign_third:
 			if(isSign){
 				MyToast.showLong(this, "正在签到中，请稍等...");
 				return;
 			}
+			removeRunnable();
 			isSign = true;
 			asyncSFZ = new AsyncParseSFZ(this,onReadSFZListener);
 			asyncSFZ.execute(SFZ.THIRD);
@@ -215,6 +258,7 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 			UnCommitInfoDao dao=new UnCommitInfoDao(MainActivity.this);
 			dao.save(signObject);
 		}
+		postRunnable();
 	}
 	
 	@Override
@@ -224,7 +268,13 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 			SerialPortManager.getInstance().openSerialPort();
 		}
 		Log.i("MainActivity", "onResume = "+SerialPortManager.getInstance().isOpen());
-		
+		if(mReadCardHandler == null){
+			mReadCardHandler = new Handler();
+		}
+		if(mReadCardRunnable == null){
+			mReadCardRunnable = new ReadCardRunnable(this);
+		}
+		postRunnable();
 	}
 	
 	@Override
@@ -234,6 +284,7 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 		mediaPlayer.release();
 		mediaPlayer = null;
 		SerialPortManager.getInstance().closeSerialPort();
+		removeRunnable();
 	}
 	
 	private long currTime = 0;
@@ -246,6 +297,36 @@ public class MainActivity extends HBaseActivity implements OnClickListener {
 		}else{
 			AppManager.getInstance().ExitApp();
 		}
+	}
+	
+	/**
+	 * 读取二代身份证
+	 * @author Helen
+	 * 2015-10-14下午6:23:09
+	 */
+	private Handler mReadCardHandler;
+	private ReadCardRunnable mReadCardRunnable;
+	private class ReadCardRunnable implements Runnable{
+		private Context context;
+		public ReadCardRunnable(Context context){
+			this.context = context;
+		}
+		@Override
+		public void run() {
+			AsyncParseSFZ asyncSFZ = new AsyncParseSFZ(context,onReadSFZListener);
+			asyncSFZ.setShowDialog(false);
+			asyncSFZ.execute(SFZ.SECOND);
+		}
+		
+	}
+	//private boolean isCancel = false;//是否取消自动读取二代身份证
+	private void postRunnable(){
+		removeRunnable();
+		mReadCardHandler.postDelayed(mReadCardRunnable, 1000*2);
+	}
+	
+	private void removeRunnable(){
+		mReadCardHandler.removeCallbacks(mReadCardRunnable);
 	}
 
 }
