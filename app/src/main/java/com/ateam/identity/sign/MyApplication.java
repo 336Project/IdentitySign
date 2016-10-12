63ef67522200411e4ea959bbb27b92ff13df229a
lixiaowei <lixiaowei@91yong.com>
2016-10-12 16:51:31 +0800

    添加权限控制

--- app/src/main/java/com/ateam/identity/sign/MyApplication.java
+++ app/src/main/java/com/ateam/identity/sign/MyApplication.java
@@ -1,15 +1,15 @@
 package com.ateam.identity.sign;
 
+import android.content.Intent;
+import android.util.Log;
+
 import com.ateam.identity.sign.moduel.User;
 import com.ateam.identity.sign.service.UnSignService;
 import com.ateam.identity.sign.util.CrashHandler;
 import com.ateam.identity.sign.util.Installation;
+import com.helen.andbase.application.HBaseApplication;
 
-import android.app.Application;
-import android.content.Intent;
-import android.util.Log;
-
-public class MyApplication extends Application{
+public class MyApplication extends HBaseApplication{
 	private User user;
 	private String classroom;
 	@Override
